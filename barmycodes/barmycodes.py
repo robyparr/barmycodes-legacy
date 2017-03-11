01e4f56f4fed80a4536977462d5d4d1bd6716527
Roby Parr <roby.parr@gmail.com>
2017-03-11 19:47:49 +0900

    PNG generation API and a bit of refactoring.

--- barmycodes/barmycodes.py
+++ barmycodes/barmycodes.py
@@ -11,6 +11,41 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
+def _get_barcodes(args, ignore_dimensions=True):
+    """Generate barcodes from the request arguments.
+    Utility method for the routes.
+    """
+
+    # Values to generate barcodes from
+    barcode_values = args.getlist('b[]')
+    barcode_type = args.get('type', 'Code128')
+
+    if not barcode_type in ('Code128', 'QR'):
+        barcode_type = 'Code128'
+
+    # Determine PDF size params
+    measurement = args.get('measurement')
+    width = args.get('width')
+    height = args.get('height')
+
+    # Generate barcodes
+    barcodes = []
+    for value in barcode_values:
+        if not value:
+            continue
+
+        # Generate the barcode
+        barcode = Barcode(
+            barcode_type,
+            value,
+            width=(width if width and not ignore_dimensions else None),
+            height=(height if height and not ignore_dimensions else None),
+            unit=(measurement if measurement in ('inch', 'mm') and not ignore_dimensions else 'mm')
+        )
+
+        barcodes.append(barcode)
+
+    return barcodes
 
 # Routes
 @app.route("/", methods=['GET'])
@@ -20,29 +55,44 @@ def index():
     be generated from the values.
     """
 
-    # Values to generate barcodes from
-    barcode_values = request.args.getlist('b[]')
-    barcode_type = request.args.get('type', 'Code128')
-
-    if not barcode_type in ('Code128', 'QR'):
-        barcode_type = 'Code128'
-
-    # List of generated barcodes
-    barcodes = []
-
-    # Create barcodes for each barcode value
-    for value in barcode_values:
-        if not value:
-            continue
-        barcode = Barcode(barcode_type, value, unit='mm')
-        barcodes.append(barcode)
+    barcodes = _get_barcodes(request.args)
 
     # Render the template
     data = {
         'barcodes': barcodes,
-        'barcode_values': '\n'.join(barcode_values)
+        'barcode_values': '\n'.join([b.text_value for b in barcodes])
     }
     return render_template('index.html', data=data)
+
+
+@app.route("/png", methods=['GET'])
+def png():
+    """Generate a PNG image for a single barcode."""
+
+    # Get a single barcode's information
+    barcode_value = request.args.get('b[]')
+    barcode_type = request.args.get('type', 'Code128')
+
+    if not barcode_type in ['Code128', 'QR']:
+        barcode_type = 'Code128'
+
+    # Make sure we have a value
+    if not barcode_value:
+        return redirect('/')
+
+    # Generate the barcode
+    barcode = Barcode(barcode_type, barcode_value)
+
+    # Create the response
+    response = Response(
+        barcode.as_string('png'),
+        mimetype='image/png'
+    )
+
+    response.headers['Content-Disposition'] = 'attachment; filename=barmycodes.png'
+
+    return response
+
 
 @app.route("/pdf", methods=['GET'])
 def pdf():
@@ -50,17 +100,7 @@ def pdf():
     querystring params 'b[].'
     """
 
-    # Values to generate barcodes from
-    barcode_values = request.args.getlist('b[]')
-    barcode_type = request.args.get('type', 'Code128')
-
-    if not barcode_type in ('Code128', 'QR'):
-        barcode_type = 'Code128'
-
-    # Determine PDF size params
-    measurement = request.args.get('measurement')
-    width = request.args.get('width')
-    height = request.args.get('height')
+    barcodes = _get_barcodes(request.args, ignore_dimensions=False)
 
     # Start PDF generation
     buffer = BytesIO()
@@ -68,19 +108,7 @@ def pdf():
     pdf.setTitle('barmycodes.pdf')
 
     # Generate barcodes and add to PDF
-    for value in barcode_values:
-        if not value:
-            continue
-
-        # Generate the barcode
-        barcode = Barcode(
-            barcode_type,
-            value,
-            width=(width if width else None),
-            height=(height if height else None),
-            unit=(measurement if measurement in ('inch', 'mm') else 'mm')
-        )
-
+    for barcode in barcodes:
         # Add the barcode to the PDF
         barcode_buffer = BytesIO(barcode.as_string('png'))
         pdf.drawImage(ImageReader(barcode_buffer), 1, 1)
