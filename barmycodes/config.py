702105dc92c6b91db2d49129c53e2a5ff5cb3c82
Roby Parr <roby.parr@gmail.com>
2018-07-22 18:33:05 -0400

    Replace Adsense with Chikita ads

--- barmycodes/config.py
+++ barmycodes/config.py
@@ -5,6 +5,4 @@ class Config(object):
     USE_CONSENT = os.getenv('USE_CONSENT', False)
     CONSENT_FLOW_ID = os.getenv('CONSENT_FLOW_ID', '')
     SHOW_ADS = os.getenv('SHOW_ADS', False)
-    AD_CLIENT = os.getenv('AD_CLIENT', '')
-    AD_SLOT = os.getenv('AD_SLOT', '')
     SHOW_PRIVACY_POLICY = os.getenv('SHOW_PRIVACY_POLICY', False)
