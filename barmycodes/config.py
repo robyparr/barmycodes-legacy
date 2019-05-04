f270b7dc6bb8c12a29e2328b654b3276935f92ed
Roby Parr <roby.parr@gmail.com>
2019-05-04 16:09:02 +0900

    Move Google Analytics code into config

--- barmycodes/config.py
+++ barmycodes/config.py
@@ -6,3 +6,4 @@ class Config(object):
     SCOUT_MONITOR = os.getenv('SCOUT_MONITOR', False)
     SCOUT_KEY = os.getenv('SCOUT_KEY', '')
     SCOUT_NAME = 'Barmycodes'
+    GOOGLE_ANALYTICS_CODE = os.getenv('GOOGLE_ANALYTICS_CODE', '')
