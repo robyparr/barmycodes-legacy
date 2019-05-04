d3147aee9ebc3f6add48ed7ae40077d1fbbe5a4c
Roby Parr <roby.parr@gmail.com>
2019-05-04 16:04:17 +0900

    Remove ads and webconsent from barmycodes

--- barmycodes/config.py
+++ barmycodes/config.py
@@ -2,9 +2,6 @@ import os
 
 class Config(object):
     DEBUG = False
-    USE_CONSENT = os.getenv('USE_CONSENT', False)
-    CONSENT_FLOW_ID = os.getenv('CONSENT_FLOW_ID', '')
-    SHOW_ADS = os.getenv('SHOW_ADS', False)
     SHOW_PRIVACY_POLICY = os.getenv('SHOW_PRIVACY_POLICY', False)
     SCOUT_MONITOR = os.getenv('SCOUT_MONITOR', False)
     SCOUT_KEY = os.getenv('SCOUT_KEY', '')
